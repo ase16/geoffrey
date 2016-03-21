d42a0239ba0cc2ae91acd700c86a962a6feaf742
Oliver Leumann <oliver.leumann@gmail.com>
2016-03-21 13:15:25 +0100

    Cleaned up routing

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,6 @@ var stormpath = require('express-stormpath');
 
 var router = express.Router();
 
-// ToDo: This is really bad code. It is sufficient for a demo, but refactor the switch point properly as soon as possible (with forwarding and role restrictions on distinct paths)
 router.get('/switchPoint', stormpath.loginRequired, function(req, res, next) {
 
 	var groupNames = [];
@@ -15,20 +14,34 @@ router.get('/switchPoint', stormpath.loginRequired, function(req, res, next) {
 			groupNames.push(groups.items[i].name);
 		}
 
-		if (groupNames.indexOf("admins") > -1) {
-
-			res.render('dashboard', {
-				title: 'Admin Dashboard',
-				text: 'Geoffrey - A company name sentiment analysis application doped with the GCE and some elastic computing'
-			});
+		if (groupNames.indexOf("admins") > -1) {	// http://expressjs.com/en/4x/api.html#res.redirect http://expressjs.com/en/guide/routing.html#response-methods
+			res.redirect('/admin/main');
 		}
 		else {
-
-			res.render('dashboard', {
-				title: 'Company Dashboard',
-				text: 'Geoffrey - A company name sentiment analysis application doped with the GCE and some elastic computing'
-			});
+			res.redirect('/company/main');
 		}
+	});
+});
+
+router.get('/admin/*', stormpath.groupsRequired(['admins']), function(req, res, next) {
+	next();
+});
+
+router.get('/admin/main', function(req, res, next) {
+	res.render('admin/main', {
+		title: 'Welcome to the Admin Dashboard ' + req.user.email,
+		text: 'Geoffrey - A company name sentiment analysis application doped with the GCE and some elastic computing'
+	});
+});
+
+router.get('/company/*', stormpath.groupsRequired(['companies']), function(req, res, next) {
+	next();
+});
+
+router.get('/company/main', function(req, res, next) {
+	res.render('company/main', {
+		title: 'Welcome to the Company Dashboard ' + req.user.email,
+		text: 'Geoffrey - A company name sentiment analysis application doped with the GCE and some elastic computing'
 	});
 });
 
