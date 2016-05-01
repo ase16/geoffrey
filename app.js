d7e722f15975e37242f2e3c13989e6e73fa9d6d1
Roy Rutishauser <roy.rutishauser@gmail.com>
2016-05-01 15:29:18 +0200

    sorry, I still need socketHandler;)

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ app.on('stormpath.ready', function() {
 	server.listen(3000, function () {
 		console.log('Geoffrey is listening on port 3000');
 		db.connect(() => {
+			socketHandler.init(server, () => console.log('Geoffrey is ready'));
+
 			// ToDo: Make Tweetfetcher indipendent (But first research for Google Bigtables)
 			/* tweetfetcher.init(db, () => {
 				socketHandler.init(server, () => console.log('Geoffrey is ready'));
